7ce92e27b36ef3e1187cff619531b4451e457595
James <james@jmew.ca>
2021-06-05 10:53:47 -0400

    (feat): add ip and port urlParams options

--- public/spectator/spectator.js
+++ public/spectator/spectator.js
@@ -6,79 +6,76 @@ let lastStudent = document.getElementById("last");
 let currentStudent = document.getElementById("current");
 let nextStudent = document.getElementById("next");
 let nextData = {};
+let connSettings = {
+  ip: "localhost",
+  port: "8181",
+};
 let updateDelay = 300;
 
-// Websocket used to communicate with the Python server backend
-let ws = new WebSocket("ws://192.168.86.96:8181");
+const startConnection = () => {
+  // Websocket used to communicate with the Python server backend
+  let ws = new WebSocket(`ws://${connSettings.ip}:${connSettings.port}`);
 
-// Generate a "random" uuid
-const uuidv4 = () => {
-  return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, (c) =>
-    (
-      c ^
-      (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))
-    ).toString(16)
-  );
-};
-
-ws.onopen = () => {
-  const message = {
-    service: "spectator",
-    data: {
-      action: "join",
-      uuid: `spectatorJoin-${uuidv4()}`,
-    },
+  // Generate a "random" uuid
+  const uuidv4 = () => {
+    return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, (c) =>
+      (
+        c ^
+        (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))
+      ).toString(16)
+    );
   };
-  ws.send(JSON.stringify(message));
-};
 
-ws.onclose = () => {
-  window.location.reload();
-};
+  ws.onopen = () => {
+    const message = {
+      service: "spectator",
+      data: {
+        action: "join",
+        uuid: `spectatorJoin-${uuidv4()}`,
+      },
+    };
+    ws.send(JSON.stringify(message));
+  };
 
-// Websocket message parsing
-ws.onmessage = (wsMessage) => {
-  console.log(wsMessage.data);
-  const msg = JSON.parse(wsMessage.data);
-  if (msg.service === undefined || msg.service === null) return;
+  ws.onclose = () => {
+    window.location.reload();
+  };
 
-  if (msg.service === "spectator") {
-    if (msg.data === undefined || msg.data === null) return;
-    const data = msg.data;
-    const lastData = { ...nextData };
-    nextData = { ...data };
-    console.log("lastData", lastData);
-    console.log("nextData", nextData);
+  // Websocket message parsing
+  ws.onmessage = (wsMessage) => {
+    const msg = JSON.parse(wsMessage.data);
+    if (msg.service === undefined || msg.service === null) return;
 
-    if (
-      Object.entries(lastData).toString() ===
-      Object.entries(nextData).toString()
-    )
-      return;
+    if (msg.service === "spectator") {
+      if (msg.data === undefined || msg.data === null) return;
+      const data = msg.data;
+      const lastData = { ...nextData };
+      nextData = { ...data };
 
-    if (
-      lastData.program &&
-      nextData.program &&
-      lastData.program !== nextData.program
-    )
-      program.innerHTML = "";
+      if (
+        Object.entries(lastData).toString() ===
+        Object.entries(nextData).toString()
+      )
+        return;
 
-    lastStudent.innerHTML = "";
-    currentStudent.innerHTML = "";
-    nextStudent.innerHTML = "";
-    setTimeout(() => {
-      program.innerHTML = nextData.program ? nextData.program : "";
-      lastStudent.innerHTML = nextData.last ? nextData.last : "";
-      currentStudent.innerHTML = nextData.current ? nextData.current : "";
-      nextStudent.innerHTML = nextData.next ? nextData.next : "";
-    }, updateDelay);
-  }
+      setTimeout(() => {
+        program.innerHTML = nextData.program ? nextData.program : "";
+        lastStudent.innerHTML = nextData.last ? nextData.last : "";
+        currentStudent.innerHTML = nextData.current ? nextData.current : "";
+        nextStudent.innerHTML = nextData.next ? nextData.next : "";
+      }, updateDelay);
+    }
+  };
 };
 
 window.onload = () => {
   const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
   if (urlParams.has("delay")) updateDelay = urlParams.get("delay");
+  if (urlParams.has("ip")) connSettings.ip = urlParams.get("ip");
+  if (urlParams.has("port")) connSettings.port = urlParams.get("port");
+
+  startConnection();
 };
 
 window.onerror = (error) => {
